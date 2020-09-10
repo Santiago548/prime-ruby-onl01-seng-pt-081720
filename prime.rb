e7161c32d5c9b9657213a74473a1000bff9105a8
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-10 21:04:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,5 @@
 # Add  code here!
-def prime?(num)
-  if num < 0 || num == 0 || num == 1
-    return false
-  else
-    (2..num-1).to_a.all? {|i| num % i != 0 }
-  end
-end
 
-puts prime?(2)
 
 #How do you create a range of numbers?
 #(1..)
